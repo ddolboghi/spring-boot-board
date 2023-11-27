0d0378a4a21f04929fec278ac6ca21d840d5c253
ddolboghi <rhdfyd128@gmail.com>
2023-11-27 22:00:58 +0900

    Feat: add Answer entity

---
+++ src/main/java/com/example/sbb/Answer.java
@@ -0,0 +1,29 @@
+package com.example.sbb;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter //프로젝트의 원활한 진행을 위해 setter 추가
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne //하나의 질문에 여러 개의 답변 가능 
+    private Question question;
+}
