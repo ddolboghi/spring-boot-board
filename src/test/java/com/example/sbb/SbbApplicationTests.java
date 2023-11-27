68aa6cf30c2fa8989d63e0e5cc262b77a6a3acf1
ddolboghi <rhdfyd128@gmail.com>
2023-11-27 22:04:16 +0900

    Test: QuestionRepository 테스트 코드 추가

--- src/test/java/com/example/sbb/SbbApplicationTests.java
+++ src/test/java/com/example/sbb/SbbApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.sbb;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void saveQuestionsTest() {
+		Question q1 = new Question();
+		q1.setSubject("what is the jpa?");
+		q1.setContent("I know that a little.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("what is the inversion of dependency?");
+		q2.setContent("I wannna konw that.");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
 	}
 
+	@Test
+	void findAllQuestionsTest() {
+		List<Question> all = this.questionRepository.findAll();
+
+		assertThat(all.size()).isEqualTo(2);
+	}
+
+	@Test
+	void findQuestionTest() {
+		Optional<Question> question = this.questionRepository.findById(1);
+        question.ifPresent(value -> assertThat(value.getSubject())
+				.isEqualTo("what is the jpa?"));
+	}
+
+	//findBySubject부터
 }
