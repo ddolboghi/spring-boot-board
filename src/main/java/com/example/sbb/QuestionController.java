96b72c79bc4fe517c8f1976ad52064aac3dc4a5f
ddolboghi <rhdfyd128@gmail.com>
2023-11-28 16:36:14 +0900

    Feat: 질문 목록 조회 뷰 생성

---
+++ src/main/java/com/example/sbb/QuestionController.java
@@ -0,0 +1,22 @@
+package com.example.sbb;
+
+import com.example.sbb.question.Question;
+import com.example.sbb.question.QuestionService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+    private final QuestionService questionService;
+
+    @GetMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionService.getList();
+        model.addAttribute("questionList", questionList);
+        return "question_list"; //템플릿 이름
+    }
+}
